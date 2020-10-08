e81898cab9d309855e005d0fe7ef9339423c1616
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-08 10:14:28 +0300

    7. toString [#371724]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,11 +1,14 @@
 package ru.job4j.tracker;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+    private String createdFormatter = created.format(formatter);
 
     public Item() {
     }
@@ -19,12 +22,20 @@ public class Item {
         this.name = name;
     }
 
+    public DateTimeFormatter getFormatter() {
+        return formatter;
+    }
+
+    public String getCreatedFormatter() {
+        return createdFormatter;
+    }
+
     @Override
     public String toString() {
         return "Item{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", created=" + created +
+                ", createdFormatter='" + createdFormatter + '\'' +
                 '}';
     }
 
--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,12 +1,9 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
-
 public class StartUI {
     public static void main(String[] args) {
         Item exapmle = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(exapmle.getCreated().format(formatter));
+        System.out.println(exapmle.getCreatedFormatter());
         System.out.println(exapmle);
     }
 }
