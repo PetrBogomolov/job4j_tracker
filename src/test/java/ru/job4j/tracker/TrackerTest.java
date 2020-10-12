720bdeb7e8d77b286bf84ec6d22b20156e40aa34
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-12 13:50:23 +0300

    6. Метод замены заявки. Tracker.replace [#371732]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -2,9 +2,7 @@ package ru.job4j.tracker;
 
 
 import org.junit.Test;
-import static org.hamcrest.Matchers.both;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
