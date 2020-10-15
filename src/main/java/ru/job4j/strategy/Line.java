0457d8bd2c62aec4db99c0ddd05e289b551301f7
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-15 11:40:48 +0300

    7. Шаблон проектирования - Стратегия [#371670]

---
+++ src/main/java/ru/job4j/strategy/Line.java
@@ -0,0 +1,8 @@
+package ru.job4j.strategy;
+
+public class Line implements Shape {
+    @Override
+    public String draw() {
+        return "___________________________________";
+    }
+}
---
+++ src/main/java/ru/job4j/strategy/Shape.java
@@ -0,0 +1,10 @@
+package ru.job4j.strategy;
+
+public interface Shape {
+
+    /**
+     * Этот метод создает рисунок
+     */
+    String draw();
+
+}
