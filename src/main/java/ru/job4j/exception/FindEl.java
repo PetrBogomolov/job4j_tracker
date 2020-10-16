7911724e0ff6ce8fb94905f54371bdf3db74c588
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-16 18:53:54 +0300

    0.4. Пользовательские исключения. [#371678]

---
+++ src/main/java/ru/job4j/exception/FindEl.java
@@ -0,0 +1,24 @@
+package ru.job4j.exception;
+
+public class FindEl {
+    public static int indexOf(String[] array, String key) throws ElementNotFoundException {
+        int result = - 1;
+        for (int index = 0; index < array.length; index++) {
+            if (array[index].equals(key)) {
+                result = index;
+            } else {
+                throw new ElementNotFoundException("Element don't found");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] array = {"element1", "element2", "element3"};
+        try {
+            indexOf(array, "element1");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
