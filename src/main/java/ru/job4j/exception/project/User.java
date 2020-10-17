517f0ae4acdbc981e9248092e10992ba4a60d9ba
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-10-17 11:24:31 +0300

    0.5. Иерархия исключений и множественный catch. [#371679]

---
+++ src/main/java/ru/job4j/exception/project/User.java
@@ -0,0 +1,23 @@
+package ru.job4j.exception.project;
+
+public class User {
+    private String userName;
+    private boolean valid;
+
+    public User(String userName, boolean valid) {
+        this.userName = userName;
+        this.valid = valid;
+    }
+
+    public User() {
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+}
---
+++ src/main/java/ru/job4j/exception/project/UserStore.java
@@ -0,0 +1,40 @@
+package ru.job4j.exception.project;
+
+public class UserStore {
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User result = new User();
+        for (User user : users) {
+            if (user.getUserName().equals(login)) {
+                result = user;
+                break;
+            } else {
+                result = null;
+            }
+        }
+        if (result == null) {
+            throw new UserNotFoundException("User was not found");
+        }
+        return result;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (user.getUserName().length() < 3 || !user.isValid()) {
+            throw new UserInvalidException("User is not valid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        try {
+            User user = findUser(users, "Petr Arsentev");
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
