dc04d3de1e5257b10fc9083f949398ed45d76e51
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-04 15:59:20 +0300

    1. Экзамен. Коллекции. [#371627]

--- src/main/java/ru/job4j/collection/Article.java
+++ src/main/java/ru/job4j/collection/Article.java
@@ -4,14 +4,12 @@ import java.util.*;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        boolean result = true;
         String[] sourse = origin.split("[\\pP\\s]");
         String[] copy = line.split("[\\pP\\s]");
         HashSet<String> check = new HashSet<>();
         HashSet<String> check2 = new HashSet<>();
         Collections.addAll(check, sourse);
         Collections.addAll(check2, copy);
-        result = check.containsAll(check2);
-        return result;
+        return check.containsAll(check2);
     }
 }
