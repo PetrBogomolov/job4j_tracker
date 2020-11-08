58205066f70c94521fcc03f7a313012b2660db3d
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-08 18:27:04 +0300

    5. Подсчет функции в диапазоне. [#371455]

---
+++ src/main/java/ru/job4j/lambda/CountFunck.java
@@ -0,0 +1,34 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class CountFunck {
+
+    public static List<Double> createLinearFunc(int start, int end) {
+        Function<Double, Double> func = doub -> (2 * doub + 1);
+        return diapason(start, end, func);
+    }
+
+    public static List<Double> createSquareFunc(int start, int end) {
+        Function<Double, Double> func = doub -> (doub * doub);
+        return diapason(start, end, func);
+    }
+
+    public static List<Double> createExponentialFunc(int start, int end) {
+        Function<Double, Double> func = doub -> Math.pow(doub, doub);
+        return diapason(start, end, func);
+    }
+
+    private static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> result = new ArrayList<>();
+        double x, y;
+        for (int i = start; i < end; i ++) {
+            x = i;
+            y = func.apply(x);
+            result.add(y);
+        }
+        return result;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/CountFunckTest.java
@@ -0,0 +1,41 @@
+package ru.job4j.lambda;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CountFunckTest {
+
+    CountFunck countFunck;
+
+    @Before
+    public void setup() {
+        countFunck = new CountFunck();
+    }
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        List<Double> result = countFunck.createLinearFunc(5, 8);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSquareFunctionThenSquareResults() {
+        List<Double> result = countFunck.createSquareFunc(5, 8);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenExponentialFunctionThenExponentialResults() {
+        List<Double> result = countFunck.createExponentialFunc(5, 8);
+        List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
+        assertThat(result, is(expected));
+    }
+}
