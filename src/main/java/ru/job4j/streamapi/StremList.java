0f8e02b60f6f5be5cd12880784554d58c02bd516
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-09 11:06:02 +0300

    check style

--- src/main/java/ru/job4j/streamapi/StremList.java
+++ src/main/java/ru/job4j/streamapi/StremList.java
@@ -6,7 +6,7 @@ import java.util.stream.Collectors;
 
 public class StremList {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(- 1, 1, - 2, 2, - 3, 3, - 4, 4, - 5, 5);
+        List<Integer> numbers = Arrays.asList(-1, 1, -2, 2, -3, 3, -4, 4, -5, 5);
         List<Integer> positive = numbers.stream()
                 .filter(Int -> Int > 0)
                 .collect(Collectors.toList());
