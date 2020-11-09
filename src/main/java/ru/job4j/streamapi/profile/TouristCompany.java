c2837c5b1e5946a47d370c6ebaef034a61af84b7
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-09 14:56:43 +0300

    3. Уникальность элементов и сортировка. [#371472]

--- src/main/java/ru/job4j/streamapi/profile/TouristCompany.java
+++ src/main/java/ru/job4j/streamapi/profile/TouristCompany.java
@@ -1,24 +1,33 @@
 package ru.job4j.streamapi.profile;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class TouristCompany {
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
+                .distinct()
                 .map(Profile::getAddress)
+                .sorted(Comparator.comparing(Address::getCity))
                 .collect(Collectors.toList());
     }
 
     public static void main(String[] args) {
         List<Profile> profiles = Arrays.asList(
-                new Profile(new Address("city", "street", 1, 1),
-                        18, "Name 1"),
-                new Profile(new Address("city", "street", 2, 2),
+                new Profile(new Address("city 3", "street", 3, 3),
+                        18, "Name 3"),
+                new Profile(new Address("city 3", "street", 3, 3),
+                        18, "Name 3"),
+                new Profile(new Address("city 2", "street", 2, 2),
                         18, "Name 2"),
-                new Profile(new Address("city", "street", 3, 3),
-                        18, "Name 3")
+                new Profile(new Address("city 2", "street", 2, 2),
+                        18, "Name 2"),
+                new Profile(new Address("city 1", "street", 1, 1),
+                        18, "Name 1"),
+                new Profile(new Address("city 1", "street", 1, 1),
+                        18, "Name 1")
         );
         collect(profiles).forEach(System.out::println);
     }
