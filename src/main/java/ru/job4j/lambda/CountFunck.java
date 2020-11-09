8122653533daec774d19bb82f34638e9907fe02e
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-09 09:08:49 +0300

    5. Подсчет функции в диапазоне. [#371455]

--- src/main/java/ru/job4j/lambda/CountFunck.java
+++ src/main/java/ru/job4j/lambda/CountFunck.java
@@ -5,23 +5,7 @@ import java.util.List;
 import java.util.function.Function;
 
 public class CountFunck {
-
-    public List<Double> createLinearFunc(int start, int end) {
-        Function<Double, Double> func = doub -> (2 * doub + 1);
-        return diapason(start, end, func);
-    }
-
-    public List<Double> createSquareFunc(int start, int end) {
-        Function<Double, Double> func = doub -> (doub * doub);
-        return diapason(start, end, func);
-    }
-
-    public List<Double> createExponentialFunc(int start, int end) {
-        Function<Double, Double> func = doub -> Math.pow(doub, doub);
-        return diapason(start, end, func);
-    }
-
-    private static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+    public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> result = new ArrayList<>();
         double x, y;
         for (int i = start; i < end; i++) {
--- src/test/java/ru/job4j/lambda/CountFunckTest.java
+++ src/test/java/ru/job4j/lambda/CountFunckTest.java
@@ -20,21 +20,21 @@ public class CountFunckTest {
 
     @Test
     public void whenLinearFunctionThenLinearResults() {
-        List<Double> result = countFunck.createLinearFunc(5, 8);
+        List<Double> result = countFunck.diapason(5, 8, x -> 2 * x + 1);
         List<Double> expected = Arrays.asList(11D, 13D, 15D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenSquareFunctionThenSquareResults() {
-        List<Double> result = countFunck.createSquareFunc(5, 8);
+        List<Double> result = countFunck.diapason(5, 8, x -> x * x);
         List<Double> expected = Arrays.asList(25D, 36D, 49D);
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
-        List<Double> result = countFunck.createExponentialFunc(5, 8);
+        List<Double> result = countFunck.diapason(5, 8, x -> Math.pow(x, x));
         List<Double> expected = Arrays.asList(3125D, 46656D, 823543D);
         assertThat(result, is(expected));
     }
