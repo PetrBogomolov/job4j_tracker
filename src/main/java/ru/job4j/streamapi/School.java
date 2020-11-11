93e73690bfa47e629c1a5f190d19890e06cceb58
PetrBogomolov <BogomoloffPD@yandex.ru>
2020-11-11 11:29:34 +0300

    0. Stream API улучшения [#371480]

--- src/main/java/ru/job4j/streamapi/School.java
+++ src/main/java/ru/job4j/streamapi/School.java
@@ -3,6 +3,7 @@ package ru.job4j.streamapi;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -16,5 +17,13 @@ public class School {
     public Map<String, Student> convert(List<Student> students) {
         return students.stream()
                 .collect(Collectors.toMap(Student::getName, e -> e, (element, dublicate) -> element));
+    }
+
+    public List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream()
+                .filter(Objects::nonNull)
+                .sorted((left, right) -> Integer.compare(right.getScope(), left.getScope()))
+                .takeWhile(student -> student.getScope() > bound)
+                .collect(Collectors.toList());
     }
 }
--- src/test/java/ru/job4j/streamapi/SchoolTest.java
+++ src/test/java/ru/job4j/streamapi/SchoolTest.java
@@ -78,4 +78,33 @@ public class SchoolTest {
         expect.put("Name 4", new  Student(40, "Name 4"));
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenSorted() {
+        List<Student> input = new ArrayList<>();
+        input.add(new Student(28, "Masha"));
+        input.add(new Student(128,"Pety"));
+        List<Student> expected = List.of(
+                new Student(128, "Pety"),
+                new Student(28, "Masha")
+        );
+        assertThat(school.levelOf(input, 20), is(expected));
+    }
+
+    @Test
+    public void whenOnlyNull() {
+        List<Student> input = new ArrayList<>();
+        input.add( null);
+        List<Student> expected = List.of();
+        assertThat(school.levelOf(input, 100), is(expected));
+    }
+
+    @Test
+    public void whenHasNull() {
+        List<Student> input = new ArrayList<>();
+        input.add( null);
+        input.add(new Student(28, "Pety"));
+        List<Student> expected = List.of(new Student(28, "Pety"));
+        assertThat(school.levelOf(input, 10), is(expected));
+    }
 }
